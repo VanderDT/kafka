2efb0eaa3d605f501bdc0ac808bbb46fb158da9f
Oleg Nadich <C:\Users\reani\AppData\Roaming\The Bat!>
2021-12-12 21:56:16 +0500

    Initial commit

---
+++ src/main/java/com/gaurav/kafka/constants/IKafkaConstants.java
@@ -0,0 +1,21 @@
+package com.gaurav.kafka.constants;
+
+public interface IKafkaConstants {
+	public static String KAFKA_BROKERS = "192.168.1.13:9092";
+	
+	public static Integer MESSAGE_COUNT=100;
+	
+	public static String CLIENT_ID="client1";
+	
+	public static String TOPIC_NAME="registrations";
+	
+	public static String GROUP_ID_CONFIG="consumerGroup10";
+	
+	public static Integer MAX_NO_MESSAGE_FOUND_COUNT=100;
+	
+	public static String OFFSET_RESET_LATEST="latest";
+	
+	public static String OFFSET_RESET_EARLIER="earliest";
+	
+	public static Integer MAX_POLL_RECORDS=1;
+}
